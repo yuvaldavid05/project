345b10aaed4e19c6c5c4dbf09781f2f250cc6b11
Yuval <yuvedavid@gmail.com>
2023-07-20 19:36:11 +0300

    added project

--- javaScript_projects/log-in/script/script.js
+++ javaScript_projects/log-in/script/script.js
@@ -4,28 +4,21 @@ function login() {
         password: document.querySelector("input[type=password]").value,
     };
 
-
-    // שליחה לשרת
     fetch("https://api.shipap.co.il/login", {
         method: 'POST',
-        credentials: 'include', // מאפשר שליחה וקבלה של עוגיות
+        credentials: 'include',
         headers: {
-            'Content-Type': 'application/json' // הגדרת סוג התוכן הנשלח לשרת
+            'Content-Type': 'application/json'
         },
-        body: JSON.stringify(obj), // תוכן הקריאה לשרת
+        body: JSON.stringify(obj),
     })
-        // קבלה מהשרת
-        // *המרת התוכן לפי הצורך*
         .then(res => res.json())
-        // התוכן שהתקבל מהשרת (לאחר טיפול של הפונקציה הקודמת)
         .then(data => {
             if (data.status == 'success') {
-                // setUser(data.user);
-                // snackbar("המשתמש התחבר בהצלחה");
+                setUser(data.user);
+                snackbar("המשתמש התחבר בהצלחה");
             } else {
-                // alert(data.message);
-
-
+                alert(data.message);
             }
         });
 }
@@ -39,6 +32,7 @@ function loginStatus() {
         .then(data => {
             if (data.status == 'success') {
                 setUser(data.user);
+                snackbar("המשתמש מחובר");
             } else {
                 setUser();
             }
@@ -46,12 +40,12 @@ function loginStatus() {
 }
 
 function logout() {
-
     fetch("https://api.shipap.co.il/logout", {
         credentials: 'include',
     })
         .then(() => {
             setUser();
+            snackbar("המשתמש התנתק בהצלחה");
         });
 }
 
@@ -75,12 +69,94 @@ function getProducts() {
                 <td contenteditable="true" oninput="contentChange(this)" class="price">${p.price}</td>
                 <td contenteditable="true" oninput="contentChange(this)" class="discount">${p.discount}</td>
                 <td>
+                <td>
+                <button class="save" onclick="saveProduct(${p.id}, this)">Save</button>
+                <button class="remove" onclick="removeProduct(${p.id}, this)">❌</button>
+                </td>
             `;
                 tbody.appendChild(tr);
             });
         })
 }
 
+
+
+function saveProduct(id, btnElem) {
+    const tr = btnElem.closest('tr');
+
+    const obj = {
+        name: tr.querySelector('.name').innerText,
+        price: tr.querySelector('.price').innerText,
+        discount: tr.querySelector('.discount').innerText,
+    };
+
+    fetch(`https://api.shipap.co.il/products/${id}`, {
+        method: 'PUT',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj),
+    })
+        .then(() => {
+
+            snackbar("המוצר נשמר בהצלחה");
+        });
+}
+
+
+
+function addProduct() {
+    const name = document.querySelector('#name');
+    const price = document.querySelector('#price');
+    const discount = document.querySelector('#discount');
+
+    const obj = {
+        name: name.value,
+        price: +price.value,
+        discount: +discount.value,
+    };
+
+    name.value = '';
+    price.value = '';
+    discount.value = '';
+
+
+
+    fetch("https://api.shipap.co.il/products", {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj),
+    })
+        .then(res => res.json())
+        .then(data => {
+            getProducts();
+            snackbar("המוצר נוסף בהצלחה");
+        });
+}
+
+
+
+function removeProduct(id, btnElem) {
+    if (!confirm('האם אתה בטוח כי ברצונך למחוק את הפריט המדובר?')) {
+        return;
+    }
+
+    fetch(`https://api.shipap.co.il/products/${id}`, {
+        method: 'DELETE',
+        credentials: 'include',
+    })
+        .then(() => {
+            btnElem.closest('tr').remove();
+            const trs = document.querySelectorAll('tbody tr');
+            trs.forEach((tr, i) => tr.querySelector('td').innerHTML = i + 1);
+
+            snackbar("המוצר נמחק בהצלחה");
+        });
+}
 
 
 function setUser(user = null) {
@@ -101,21 +177,10 @@ function setUser(user = null) {
 }
 
 
-// function loader(isShowing = false) {
-//     const loaderFrame = document.querySelector('.loaderFrame');
 
-//     if (isShowing) {
-//         loaderFrame.style.display = 'flex';
-//     } else {
-//         loaderFrame.style.display = 'none';
-//     }
-// }
-
-
-
-// function snackbar(message) {
-//     const elem = document.getElementById("snackbar");
-//     elem.innerHTML = message;
-//     elem.classList.add("show");
-//     setTimeout(() => elem.classList.remove("show"), 3000);
-// }
+function snackbar(message) {
+    const elem = document.getElementById("snackbar");
+    elem.innerHTML = message;
+    elem.classList.add("show");
+    setTimeout(() => elem.classList.remove("show"), 3000);
+}
---
+++ javaScript_projects/log-in/script/script_signUp.js
@@ -0,0 +1,37 @@
+function signUp() {
+
+    const objNew = {
+        fullName: document.querySelector("#fullName").value,
+        email: document.querySelector("#email").value,
+        userName: document.querySelector("#userName").value,
+        password: document.querySelector("input[type=password]").value,
+    }
+
+    fetch("https://api.shipap.co.il/signup", {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(objNew),
+    })
+        .then(res => res.json())
+        .then(data => {
+            if (data.status == 'success') {
+                snackbar("המשתמש נרשם בהצלחה");
+                document.querySelector(".signup").style.display = 'none';
+                document.querySelector(".success").style.display = 'block';
+            } else {
+                alert(data.message);
+            }
+        });
+}
+
+
+
+function snackbar(message) {
+    const elem = document.getElementById("snackbar");
+    elem.innerHTML = message;
+    elem.classList.add("show");
+    setTimeout(() => elem.classList.remove("show"), 3000);
+}
